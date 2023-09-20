0026e5561ef291b5b08ccf1003d62461e74a3484
P-Nth <philemonondigo@protonmail.com>
2023-09-20 15:04:53 +0300

    Initial Commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,16 +5,30 @@ const config: GatsbyConfig = {
     title: `upstr`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+    "gatsby-plugin-postcss",
+    // {
+    //   resolve: "gatsby-plugin-google-gtag",
+    //   options: {
+    //     trackingIds: [
+    //       "GOOGLE_ANALYTICS_TRACKING_ID",
+    //     ],
+    //   },
+    // },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  },
+  "gatsby-plugin-mdx",
+  "gatsby-transformer-remark",
+  "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
